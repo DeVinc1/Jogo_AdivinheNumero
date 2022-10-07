fbf7d177fc914c683f840bbf23dfd9dd3b3cf5ee
Marcos <marcos.vinicius.dev.2004@gmail.com>
2022-10-07 11:14:29 -0300

    1º Commit - Importando Bibliotecas

---
+++ src/Game.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) throws Exception {
+        System.out.println("test");
+    }
+}
