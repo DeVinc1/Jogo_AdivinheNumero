7030ebebc9115718c368e0888c0da31943631081
Marcos <marcos.vinicius.dev.2004@gmail.com>
2022-10-20 20:47:31 -0300

    Criando as funções fundamentais

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,17 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) throws Exception {
-        System.out.println("test");
+
+      
+}
+    public static int definirComecoIntervalo() {
+        /* Manipulando matematicamente o resultado de Math.random() para atender um intervalo */
+        return (int) ((Math.random() * (50 - 0)) + 0);
     }
+
+    public static int definirFinalIntervalo() {
+        return (int) ((Math.random() * (100 - 51)) + 51);
+    }
+
 }
